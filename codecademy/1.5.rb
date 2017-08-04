f9c67263a741b71d9580546d558f1b63121485b1
hanapotski <hannahjoy.zulueta@gmail.com>
2017-08-04 12:15:54 -0700

    chapter1

---
+++ codecademy/1.5.rb
@@ -0,0 +1,10 @@
+app/views/pages/home.html.erb
+
+<div class="main">
+  <div class="container">
+    <h1>Hello my name is Hannah</h1>
+    <p>I make Rails apps.</p>
+  </div>
+</div>
+
+CSS in the file in app/assets/stylesheets/pages.css.scss.
